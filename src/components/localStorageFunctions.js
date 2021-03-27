dbcaa141c747d3a75aeb8a68885aeb282948bb2a
Kevin Zhang <kevinzhang425@gmail.com>
2021-03-27 13:39:26 -0700

    Added functions for local storage

---
+++ src/components/localStorageFunctions.js
@@ -0,0 +1,28 @@
+export const isSignedIn = () => {
+    return window.localStorage.getItem("signedIn") === "true";
+}
+
+export const getGoogleId = () => {
+    return window.localStorage.getItem("googleId");
+}
+export const getEmail = () => {
+    return window.localStorage.getItem("email");
+}
+export const getPhotoUrl = () => {
+    return window.localStorage.getItem("photoUrl");
+}
+export const getDisplayName = () => {
+    return window.localStorage.getItem("displayName");
+}
+
+export const getSignedInUser = () => {
+    let localStorage = window.localStorage;
+    let signedInUser = {
+        displayName : localStorage.getItem("displayName"),
+        email: localStorage.getItem("email"),
+        googleId : localStorage.getItem("googleId"),
+        photoUrl : localStorage.getItem("photoUrl")
+    };
+
+    return signedInUser;
+}

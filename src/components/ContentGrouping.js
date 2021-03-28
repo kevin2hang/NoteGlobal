b8f039ccc7900a730044efb4935be8dc149e689b
Trevor <tr89on@gmail.com>
2021-03-27 17:52:46 -0700

    remove edit functionality

--- src/components/ContentGrouping.js
+++ src/components/ContentGrouping.js
@@ -4,20 +4,12 @@ import { isSignedIn } from './localStorageFunctions';
 
 function ContentGrouping(props) {
   const dbPath = props.path+props.name +'/';
-  const [editing, setEditing] = useState(false);
-  const [title, setTitle] = useState(props.name.split('-').join(' '));
+  const title = useState(props.name.split('-').join(' '));
 
-  const displayTitle = () => {
-    return editing 
-      ? <input value={title} onChange={(e)=>setTitle(e.target.value)}></input>
-      : <span>{title}</span>
-  }
 
   return (
     <div>
-      <h3>{displayTitle()}</h3>
-      {isSignedIn() && <button onClick={()=>setEditing(!editing)}> Edit title </button>}
-      
+      <h3>{title}</h3>      
       <ul>
         {database.ref(dbPath).on('value', (snapshot) => {
           snapshot.forEach(item => {
